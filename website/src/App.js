cd5c0707f475afeaf2815092239bb13908c97509
Chrysellia <priscilliachryso@gmail.com>
2021-12-20 15:00:07 +0300

    Start project

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.scss';
+import Navbar from './Components/Navbar/Navbar.component';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App;
